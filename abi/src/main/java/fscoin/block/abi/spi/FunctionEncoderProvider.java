b5a6194ec9e9d6484c2fd4d8067b7cadf2cc2326
GBSDFS <76682143+GBSDFS@users.noreply.github.com>
2020-12-29 17:56:14 +0800

    Add files via upload

---
+++ abi/src/main/java/fscoin/block/abi/spi/FunctionEncoderProvider.java
@@ -0,0 +1,8 @@
+
+package fscoin.block.abi.spi;
+
+import java.util.function.Supplier;
+
+import fscoin.block.abi.FunctionEncoder;
+
+public interface FunctionEncoderProvider extends Supplier<FunctionEncoder> {}
---
+++ abi/src/main/java/fscoin/block/abi/spi/FunctionReturnDecoderProvider.java
@@ -0,0 +1,9 @@
+
+package fscoin.block.abi.spi;
+
+import java.util.function.Supplier;
+
+import fscoin.block.abi.FunctionReturnDecoder;
+
+
+public interface FunctionReturnDecoderProvider extends Supplier<FunctionReturnDecoder> {}
